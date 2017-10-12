c48e3b8373ab4f359f9f168af43dbc06a0ac91f8
pavel1yakimovich <pavel1yakimovich@gmail.com>
2017-10-12 23:41:17 +0300

    01AdvancedCSharp: file and directories search added

---
+++ Task1AdvancedCSharp/ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Task1AdvancedCSharp;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fsv = new FileSystemVisitor
+            {
+                RootDirectory = "E:\\testfolder"
+            };
+
+            foreach (var item in fsv)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Task1AdvancedCSharp/Task1AdvancedCSharp/FileSystemVisitor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Task1AdvancedCSharp
+{
+    public class FileSystemVisitor
+    {
+        public string RootDirectory { get; set; }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            var directories = Directory.GetDirectories(RootDirectory);
+            var files = Directory.GetFiles(RootDirectory);
+            foreach (var dir in directories)
+            {
+                yield return dir;
+                var fsv = new FileSystemVisitor
+                {
+                    RootDirectory = dir
+                };
+                foreach (var item in fsv)
+                {
+                    yield return item;
+                }
+            }
+            foreach (var file in files)
+            {
+                yield return file;
+            }
+        }
+    }
+}
