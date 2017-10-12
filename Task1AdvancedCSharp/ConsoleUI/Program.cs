664672a40f5fe586b8dc8398e3565202959afc28
pavel1yakimovich <pavel1yakimovich@gmail.com>
2017-10-13 00:02:24 +0300

    01AdvancedCSharp: filtration added

--- Task1AdvancedCSharp/ConsoleUI/Program.cs
+++ Task1AdvancedCSharp/ConsoleUI/Program.cs
@@ -7,7 +7,19 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            var fsv = new FileSystemVisitor
+            var fsv = new FileSystemVisitor()
+            {
+                RootDirectory = "E:\\testfolder"
+            };
+
+            foreach (var item in fsv)
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.WriteLine("With filtration - shouldn't contain \"2\"");
+
+            fsv = new FileSystemVisitor(s => s.Contains("2"))
             {
                 RootDirectory = "E:\\testfolder"
             };
--- Task1AdvancedCSharp/Task1AdvancedCSharp/FileSystemVisitor.cs
+++ Task1AdvancedCSharp/Task1AdvancedCSharp/FileSystemVisitor.cs
@@ -8,26 +8,61 @@ namespace Task1AdvancedCSharp
     public class FileSystemVisitor
     {
         public string RootDirectory { get; set; }
+        
+        private Predicate<string> filter;
+
+        public FileSystemVisitor() { }
+        /// <summary>
+        /// .ctor with filter delegate
+        /// </summary>
+        /// <param name="filter">Should return true, if exclude file or directory</param>
+        public FileSystemVisitor(Predicate<string> filter)
+        {
+            this.filter = filter;
+        }
 
         public IEnumerator<string> GetEnumerator()
         {
             var directories = Directory.GetDirectories(RootDirectory);
             var files = Directory.GetFiles(RootDirectory);
+
+            //Searching in directories
             foreach (var dir in directories)
             {
-                yield return dir;
-                var fsv = new FileSystemVisitor
+                //if the dir is excluded, do not search in this dir
+                if (filter == null || !filter(dir))
                 {
-                    RootDirectory = dir
-                };
-                foreach (var item in fsv)
-                {
-                    yield return item;
+                    yield return dir;
+
+                    FileSystemVisitor fsv;
+                    if (filter != null)
+                    {
+                        fsv = new FileSystemVisitor(filter);
+                    }
+                    else
+                    {
+                        fsv = new FileSystemVisitor();
+                    }
+                    fsv.RootDirectory = dir;
+
+                    //Searching in subdirectories
+                    foreach (var item in fsv)
+                    {
+                        if (filter == null || !filter(item))
+                        {
+                            yield return item;
+                        }
+                    }
                 }
             }
+
+            //Return files
             foreach (var file in files)
             {
-                yield return file;
+                if (filter == null || !filter(file))
+                {
+                    yield return file;
+                }
             }
         }
     }
