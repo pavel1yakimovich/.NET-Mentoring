88d14d902c6f88d65734c30967dfa185242cfd81
pavel1yakimovich <pavel1yakimovich@gmail.com>
2017-10-22 20:39:00 +0300

    task 1 added

---
+++ Module2ExceptionHandling/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a string.");
+                var str = Console.ReadLine();
+                try
+                {
+                    Console.WriteLine(str[0]);
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("You entered empty string.");
+                }
+            }
+        }
+    }
+}
