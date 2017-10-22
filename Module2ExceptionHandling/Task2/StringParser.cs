e76405d950fce89932a7ac61424471719adde937
pavel1yakimovich <pavel1yakimovich@gmail.com>
2017-10-22 21:18:10 +0300

    Task 2 added

---
+++ Module2ExceptionHandling/Task2/StringParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Task2
+{
+    public static class StringParser
+    {
+        public static int StrToInt(string str)
+        {
+            int number = 0;
+            bool positive = true;
+
+            switch (str[0])
+            {
+                case '+':
+                    str = str.Remove(0, 1);
+                    positive = true;
+                    break;
+                case '-':
+                    str = str.Remove(0, 1);
+                    positive = false;
+                    break;
+            }
+
+            int length = str.Count();
+
+            for (int i = 0; i < length; i++)
+            {
+                var strNumber = str[i] - '0';
+                if (strNumber > 9 || strNumber < 0)
+                {
+                    throw new InvalidCastException("String doesn't contain number.");
+                }
+                number += strNumber * (int)Math.Pow(10, length - i - 1);
+            }
+
+            return positive ? number : -number;
+        }
+    }
+}
