bd17d51f9f8c65df07e124934c7a7ba6f19e03e7
Unknown <pavel1yakimovich@gmail.com>
2017-11-08 14:49:00 +0300

    FileSustemWatcher added

--- Module4BCL/Program.cs
+++ Module4BCL/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace Module4BCL
     {
         static void Main(string[] args)
         {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = "D:\\testfolder";
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Filter = "*.*";
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine("Press \'q\' to quit the sample.");
+            while (Console.Read() != 'q') ;
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.Name.Contains("."))
+            {
+                Console.WriteLine("File found");
+
+
+            }
         }
     }
 }
