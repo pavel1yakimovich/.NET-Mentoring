a9662d188198cd836c62ba20f0c9e56adfcddfed
Unknown <pavel1yakimovich@gmail.com>
2017-11-08 16:23:57 +0300

    Customsections added

---
+++ Module4BCL/FileSystemWatcherConfigurationSection.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Xml;
+using System.Configuration;
+using System.Globalization;
+
+namespace Module4BCL
+{ 
+    public class FileSystemWatcherConfigurationSection : ConfigurationSection
+    {
+        [ConfigurationProperty("culture")]
+        public CultureInfo Culture
+        {
+            get => (CultureInfo)(this["culture"]);
+            set { this["culture"] = value; }
+        }
+
+        [ConfigurationProperty("folders")]
+        [ConfigurationCollection(typeof(FoldersElementCollectoin), AddItemName = "folder")]
+        public FoldersElementCollectoin Folders
+        {
+            get => (FoldersElementCollectoin)(this["folders"]);
+            set { this["folders"] = value; }
+        }
+
+        [ConfigurationProperty("rules")]
+        [ConfigurationCollection(typeof(FoldersElementCollectoin), AddItemName = "rule")]
+        public RulesElementCollectoin Rules
+        {
+            get => (RulesElementCollectoin)(this["rules"]);
+            set { this["rules"] = value; }
+        }
+    }
+
+    public class FoldersElementCollectoin : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new FolderElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return (element as FolderElement).Name;
+        }
+    }
+
+    public class FolderElement : ConfigurationElement
+    {
+        [ConfigurationProperty("name", IsKey = true)]
+        public string Name
+        {
+            get { return this["name"] as string; }
+            set { this["name"] = value; }
+        }
+    }
+
+    public class RulesElementCollectoin : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new RuleElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return (element as RuleElement);
+        }
+    }
+
+    public class RuleElement : ConfigurationElement
+    {
+
+        [ConfigurationProperty("template", IsKey = true)]
+        public string Template
+        {
+            get => (string)(this["template"]);
+            set { this["template"] = value; }
+        }
+
+        [ConfigurationProperty("folder")]
+        public string Folder
+        {
+            get => (string)(this["folder"]);
+            set { this["folder"] = value; }
+        }
+
+        [ConfigurationProperty("addIndex")]
+        public bool AddIndex
+        {
+            get => (bool)(this["addIndex"]);
+            set { this["addIndex"] = value; }
+        }
+
+        [ConfigurationProperty("addDate")]
+        public bool AddDate
+        {
+            get => (bool)(this["addDate"]);
+            set { this["addDate"] = value; }
+        }
+    }
+}
--- Module4BCL/Program.cs
+++ Module4BCL/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,8 +11,11 @@ namespace Module4BCL
 {
     class Program
     {
+        private List<string> folders;
         static void Main(string[] args)
         {
+            List<string> folders;
+            ConfigurateApp(out folders);
             FileSystemWatcher watcher = new FileSystemWatcher();
             watcher.Path = "D:\\testfolder";
             watcher.NotifyFilter = NotifyFilters.LastWrite;
@@ -30,6 +35,16 @@ namespace Module4BCL
 
 
             }
+        }
+
+        private static void ConfigurateApp(out List<string> folders)
+        {
+            folders = new List<string>();
+            var asm = Assembly.GetExecutingAssembly();
+            var section = (ConfigurationManager.GetSection("fileSystemWatcher") as FileSystemWatcherConfigurationSection);
+            foreach (var item in section.Folders)
+                Console.WriteLine((item as FolderElement).Name);
+            var culture = section.Culture;
         }
     }
 }
