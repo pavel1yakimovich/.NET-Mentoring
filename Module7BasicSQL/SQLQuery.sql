f984203e0a65f98a5389368deb11279bf65a2fdf
Unknown <pavel1yakimovich@gmail.com>
2017-11-20 14:12:11 +0300

    Task 3 added

--- Module7BasicSQL/SQLQuery.sql
+++ Module7BasicSQL/SQLQuery.sql
@@ -1,8 +1,8 @@
 --Task 1
-DECLARE @dt DateTime = CONVERT(DateTime,'05/01/1998',101)
+DECLARE @dt DateTime = CONVERT(DateTime,'05/06/1998',101)
 SELECT OrderID, ShippedDate, ShipVia
 FROM Orders
-WHERE ShippedDate > @dt AND ShipVia >= 2
+WHERE ShippedDate >= @dt AND ShipVia >= 2
 
 --Task 2
 SELECT OrderID, ShippedDate =
@@ -13,3 +13,11 @@ FROM Orders
 WHERE ShippedDate IS NULL
 
 --Task 3
+DECLARE @dt3 DateTime = CONVERT(DateTime,'05/06/1998',101)
+SELECT OrderID as [Order Number], 'Shipped Date' = 
+CASE
+	WHEN [ShippedDate] IS NULL THEN 'Not Shipped'
+	ELSE CONVERT(VARCHAR, [ShippedDate], 121)
+END
+FROM Orders
+WHERE ShippedDate > @dt3 OR ShippedDate IS NULL
