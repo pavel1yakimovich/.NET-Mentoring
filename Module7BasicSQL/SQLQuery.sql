32aee781b138bb883db4f11f9d5f2582ae5bc23e
Unknown <pavel1yakimovich@gmail.com>
2017-11-20 14:12:34 +0300

    Tasks renamed

--- Module7BasicSQL/SQLQuery.sql
+++ Module7BasicSQL/SQLQuery.sql
@@ -1,10 +1,10 @@
---Task 1
+--Task 1.1
 DECLARE @dt DateTime = CONVERT(DateTime,'05/06/1998',101)
 SELECT OrderID, ShippedDate, ShipVia
 FROM Orders
 WHERE ShippedDate >= @dt AND ShipVia >= 2
 
---Task 2
+--Task 1.2
 SELECT OrderID, ShippedDate =
 CASE
 	WHEN ShippedDate IS NULL THEN 'Not Shipped'
@@ -12,7 +12,7 @@ END
 FROM Orders
 WHERE ShippedDate IS NULL
 
---Task 3
+--Task 1.3
 DECLARE @dt3 DateTime = CONVERT(DateTime,'05/06/1998',101)
 SELECT OrderID as [Order Number], 'Shipped Date' = 
 CASE
