1affb32c8c5757863facc7f9f328eb42f269bd9b
Unknown <pavel1yakimovich@gmail.com>
2017-11-23 13:43:46 +0300

    Tasks 3.2 and 3.3 added

---
+++ Module7BasicSQL/NorthWind1.1/obj/Debug/postdeploy.sql
@@ -0,0 +1,16 @@
+﻿
+IF NOT EXISTS (SELECT * FROM sys.objects 
+	WHERE object_id = OBJECT_ID(N'[dbo].[CreditCards]'))
+BEGIN
+--The following statement was imported into the database project as a schema object and named dbo.CreditCards.
+CREATE TABLE [dbo].[CreditCards](
+	[CreditCardID] [int] IDENTITY(1,1) NOT NULL,
+	[ExpireDate] [DateTime] NOT NULL,
+	[CardHolder] [nvarchar](50) NULL,
+	[CustomerID] [nchar](5) NOT NULL,
+	CONSTRAINT [PK_CreditCards] PRIMARY KEY ( [CreditCardID] ASC),
+	CONSTRAINT [FK_CreditCards_Customers] FOREIGN KEY ( [CustomerID] ) REFERENCES [dbo].[Customers] (CustomerID)
+)
+ 
+END
+GO
---
+++ Module7BasicSQL/NorthWind1.3/dbo/Tables/CreditCards.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CreditCards](
+	[CreditCardID] [int] IDENTITY(1,1) NOT NULL,
+	[ExpireDate] [DateTime] NOT NULL,
+	[CardHolder] [nvarchar](50) NULL,
+	[CustomerID] [nchar](5) NOT NULL,
+	CONSTRAINT [PK_CreditCards] PRIMARY KEY ( [CreditCardID] ASC),
+	CONSTRAINT [FK_CreditCards_Customers] FOREIGN KEY ( [CustomerID] ) REFERENCES [dbo].[Customers] (CustomerID)
+)
