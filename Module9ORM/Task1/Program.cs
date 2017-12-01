27c5c010c39e5a4aa607e4c1102661fb3d9e0a13
Unknown <pavel1yakimovich@gmail.com>
2017-12-01 17:11:53 +0300

    Task 2.3 added

--- Module9ORM/Task1/Program.cs
+++ Module9ORM/Task1/Program.cs
@@ -19,23 +19,27 @@ namespace Task1
                 var products = db.Products.LoadWith(r => r.Category).LoadWith(r => r.Supplier).ToList();
                 var employees = db.Employees.Join(db.EmployeeTerritories, e => e.EmployeeID, et => et.EmployeeID, (e, et) => new { e, et })
                     .Join(db.Territories, et => et.et.TerritoryID, t => t.TerritoryID, (et, t) => new { et, t })
-                    .Join(db.Regions, t => t.t.RegionID, r => r.RegionID, (t, r) => new { t, r });
-                
+                    .Join(db.Regions, t => t.t.RegionID, r => r.RegionID, (t, r) => new { t, r })
+                    .GroupBy(r => r.t.et.e.FirstName + r.t.et.e.LastName, r => r.r.Description, (r, e) => new { Id = r, Region = e.FirstOrDefault() });
+
                 foreach (var item in products)
                 {
                     Console.WriteLine($"Product: {item.Name} Category: {item.Category.Name} Supplier: {item.Supplier.Name}");
                 }
-
-                //hack
-                var empIds = new List<int?>();
+                
                 foreach (var item in employees)
                 {
-                    var empId = item?.t?.et?.e?.EmployeeID;
-                    if (!empIds.Contains(empId))
-                    {
-                        empIds.Add(empId);
-                        Console.WriteLine($"Employee: {item?.t?.et?.e?.FirstName} {item?.t?.et?.e?.LastName} Region: {item?.r?.Description}");
-                    }
+                        Console.WriteLine($"Employee: {item?.Id} Region: {item?.Region}");
+                }
+                
+                var employees2 = db.Employees.Join(db.EmployeeTerritories, e => e.EmployeeID, et => et.EmployeeID, (e, et) => new { e, et })
+                    .Join(db.Territories, et => et.et.TerritoryID, t => t.TerritoryID, (et, t) => new { et, t })
+                    .Join(db.Regions, t => t.t.RegionID, r => r.RegionID, (t, r) => new { t, r })
+                    .GroupBy(r => r.r.Description, r => r.t.et.e.EmployeeID, (r, e) => new { Region = r, Employees = e.Count() });
+
+                foreach (var item in employees2)
+                {
+                    Console.WriteLine($"Region: {item?.Region} Employees: {item?.Employees}");
                 }
             }
             #endregion
