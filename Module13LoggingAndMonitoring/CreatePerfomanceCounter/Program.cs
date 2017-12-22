22e1c00c52722f011619c6817f629183d39b2df7
Unknown <pavel1yakimovich@gmail.com>
2017-12-22 15:20:58 +0300

    Module 13 task 1 added

---
+++ Module13LoggingAndMonitoring/CreatePerfomanceCounter/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatePerfomanceCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!PerformanceCounterCategory.Exists("Custom category"))
+            {
+                var counterCreationDataCollection = new CounterCreationDataCollection();
+
+                var loginCounter = new CounterCreationData()
+                {
+                    CounterName = "Log in counter",
+                    CounterType = PerformanceCounterType.NumberOfItems32
+                };
+                counterCreationDataCollection.Add(loginCounter);
+
+                var logoffCounter = new CounterCreationData()
+                {
+                    CounterName = "Log off counter",
+                    CounterType = PerformanceCounterType.NumberOfItems32
+                };
+                counterCreationDataCollection.Add(logoffCounter);
+
+                var homeIndexCounter = new CounterCreationData()
+                {
+                    CounterName = "Calling Home/Index",
+                    CounterType = PerformanceCounterType.NumberOfItems32
+                };
+                counterCreationDataCollection.Add(homeIndexCounter);
+
+                try
+                {
+                    PerformanceCounterCategory.Create(
+                        "Custom category", "",
+                        PerformanceCounterCategoryType.MultiInstance,
+                        counterCreationDataCollection);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    Console.WriteLine("Do not have permissions");
+                    Console.Read();
+                }
+
+                Console.WriteLine("Category created");
+                Console.Read();
+            }
+            else
+            {
+                Console.WriteLine("This counter already exists");
+                Console.Read();
+            }
+        }
+    }
+}
